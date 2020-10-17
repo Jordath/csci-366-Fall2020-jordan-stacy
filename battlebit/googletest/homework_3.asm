a9fd9d8c0b37b086910348e71c9a2af1da627c0e
Jordath <sonofthefrozenmoon@yahoo.com>
2020-10-17 14:46:23 -0600

    Got problems 1 and 2 done with Homework 3. Problem 3 (homework_3.asm) though... Holy Moses... I created a find_max function on godbolt.org in C, copy and pasted the code into the .asm file to interpret the code to get it to work, but I have no idea what I'm doing. Stopping for now.

--- battlebit/googletest/homework_3.asm
+++ battlebit/googletest/homework_3.asm
@@ -3,10 +3,34 @@
 find_max:
         ;; rdi has the array in it
         ;; rsi has the length in it
-        mov rax, [rdi] ;; move the first value in the array into rax
+        ;;mov rax, [rdi] ;; move the first value in the array into rax
         ;; you will need to loop through the array and
         ;; compare each value with rax to determine if it is greater
         ;; after the comparison, decrement the count, bump the
         ;; array pointer by 8 (long long = 64 bits = 8 bytes)
         ;; and if the counter is greater than zero, loop
+        push    rbp
+        mov     rbp, rsp
+        mov     QWORD PTR [rbp-24], rdi
+        mov     DWORD PTR [rbp-28], esi
+        mov     rax, QWORD PTR [rbp-24]
+        mov     rax, QWORD PTR [rax]
+        mov     QWORD PTR [rbp-8], rax
+.L4:
+        cmp     DWORD PTR [rbp-28], 0
+        jle     .L2
+        add     QWORD PTR [rbp-24], 8
+        sub     DWORD PTR [rbp-28], 1
+        mov     rax, QWORD PTR [rbp-24]
+        mov     rax, QWORD PTR [rax]
+        mov     QWORD PTR [rbp-16], rax
+        mov     rax, QWORD PTR [rbp-8]
+        cmp     rax, QWORD PTR [rbp-16]
+        jge     .L4
+        mov     rax, QWORD PTR [rbp-16]
+        mov     QWORD PTR [rbp-8], rax
+        jmp     .L4
+.L2:
+        mov     rax, QWORD PTR [rbp-8]
+        pop     rbp
         ret
